99996898f99c1d84e7a9e079b4f8bb66df0e7ee0
James Martin <jamesml@live.unc.edu>
2013-11-17 19:37:06 -0500

    Basic index page completed.

---
+++ site/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	console.log("Page loaded.")
+});
+
+$(document).on("click", "#submit", function(){
+	var building = $("#b-building").val();
+	var floor = $("#b-floor").val();
+	var room = $("#b-room").val();
+
+	//If all values are set back to default value
+	if(building==="Any" && floor==="Any" && room==="Any"){
+		//console.log("Values are default");
+		$("#bathroom-list").empty();
+		$("#bathroom-list").hide();
+	}
+});
