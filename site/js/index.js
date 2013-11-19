c9a8df67ea672c4f7a06ff16c65db562a157644f
James Martin <jamesml@live.unc.edu>
2013-11-19 15:24:49 -0500

    Made changes to landing page to simplify. Users no longer have the choice of selecting a room, bathrooms will be populated into well from Building/Floor options. Hard coded data into dropdown menus. Will query database based on selected values, and if no bathrooms are found for that building/floor, then returns an empty list.

--- site/js/index.js
+++ site/js/index.js
@@ -1,21 +1,27 @@
-var selectedRoom;
+var cu
+var buildings = new Array();
 
 $(document).ready(function() {
 	console.log("Page loaded.")
+
+	$.ajax()
 });
 
 $(document).on("click", "#submit", function(){
 	var building = $("#b-building").val();
 	var floor = $("#b-floor").val();
-	var room = $("#b-room").val();
 
 	//If all values are set back to default value
-	if(building==="Any" && floor==="Any" && room==="Any"){
-		//console.log("Values are default");
+	if(building==="None Selected" && floor==="None Selected"){
+		console.log("Values are default");
 		$("#bathroom-list").empty();
-		$("#bathroom-list").hide();
+		$("#bathroom-list").toggleClass("hidden");
 	} else {
 		selectedRoom = building + " " + floor + " " + room;
 		console.log(selectedRoom);
 	}
+});
+
+$(document).on("change", "#b-building", function(){
+
 });
